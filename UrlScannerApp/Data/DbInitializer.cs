97ac78ddf14d543928307cd76d0fad314b385cfa
Thomas Green <Thomas.Green@aldautomotive.com>
2021-05-05 12:27:04 +0100

    Create webapp with data initialization

---
+++ UrlScannerApp/Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using UrlScannerApp.Data.Models;
+
+namespace UrlScannerApp.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(UrlScannerDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Records.Any())
+            {
+                return;
+            }
+
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "UrlScannerApp.Data.records.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+                    {
+                        HasHeaderRecord = true,
+                        HeaderValidated = null,
+                        MissingFieldFound = null
+                    });
+                    var records = csvReader.GetRecords<Record>().ToArray();
+                    context.Records.AddRange(records);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ UrlScannerApp/Data/UrlScannerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlScannerApp.Data.Models;
+
+namespace UrlScannerApp.Data
+{
+    public class UrlScannerDbContext : DbContext
+    {
+        public UrlScannerDbContext(DbContextOptions<UrlScannerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+}
